f295767f7ef045e0cf9052de595b002a48a2b3cb
Amataverna Lee <amataverna@gmail.com>
2019-02-22 16:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ function takeANumber(katzDeliLine,name){
   
 }
 
-
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name);
+  var line = katzDeliLine.length;
+  
+  return("Welcome, " + name +". You are number " + line + " in line.");
+  
+}
 
 
 
